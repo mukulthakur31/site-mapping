7131dabf5fc0afd853157e510e7fef6084393e94
mukulthakur31 <mukulthakur01234@gmail.com>
2025-03-11 14:10:16 +0530

    first

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import ProductView from "./ProductView";
+import Footer from "./Footer";
+import Store from "./Store";
+import SingleProduct from "./SingleProduct";
+import InputBox from "./InputBox";
+export{
+    Navbar,
+    ProductView,
+    Footer,
+    Store,
+    SingleProduct,
+    InputBox
+}
---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import Homepage from "./Homepage";
+import Cartpage from "./Cartpage";
+import Checkout from "./Checkout";
+import Profile from "./Profile";
+import WishListPage from "./WishListPage";
+import ViewProduct from "./ViewProduct";
+import OrderPage from "./OrderPage";
+import Login from "./Login";
+import Signup from "./Signup";
+import ForgetPassword from "./ForgetPassword";
+export {
+    Homepage,
+    Cartpage,
+    Checkout,
+    Profile,
+    WishListPage,
+    ViewProduct,
+    OrderPage,
+    Login,
+    Signup,
+    ForgetPassword
+}
